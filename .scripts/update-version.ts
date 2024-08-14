161d54be03bb448b6e275d4b2b1e9b4430b56b06
jxom <jakemoxey@gmail.com>
2024-08-14 13:20:59 +1000

    chore: version

--- .scripts/update-version.ts
+++ .scripts/update-version.ts
@@ -1,8 +1,15 @@
 import { join } from 'node:path'
 import fs from 'fs-extra'
 
+// Writes the current package.json version to `./src/errors/version.ts`.
+const versionFilePath = join(import.meta.dirname, '../src/version.ts')
 const packageJsonPath = join(import.meta.dirname, '../src/package.json')
 const packageJson = fs.readJsonSync(packageJsonPath)
+
+fs.writeFileSync(
+  versionFilePath,
+  `/** @internal */\nexport const version = '${packageJson.version}'\n`,
+)
 
 // Update JSR version.
 const jsrJsonPath = join(import.meta.dirname, '../src/jsr.json')
