64da2d658e258b344aa3b3d348f9a485d1feecf2
jxom <jakemoxey@gmail.com>
2024-08-16 15:54:54 +1000

    chore: rm

--- src/Hash.ts
+++ src/Hash.ts
@@ -2,8 +2,6 @@ export { isHash } from './internal/hash/isHash.js'
 
 export { keccak256 } from './internal/hash/keccak256.js'
 
-export { normalizeSignature } from './internal/solidity/normalizeSignature.js'
-
 export { ripemd160 } from './internal/hash/ripemd160.js'
 
 export { sha256 } from './internal/hash/sha256.js'
