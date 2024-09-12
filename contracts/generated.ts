0be06caaf7b156152ef63c8b8b8376458287303a
jxom <jxom@users.noreply.github.com>
2024-09-12 22:57:30 +0000

    chore: format

--- contracts/generated.ts
+++ contracts/generated.ts
@@ -1,313 +1,309 @@
 export const NonConformingEvents = {
-  abi: [
+  "abi": [
     {
-      type: 'function',
-      name: 'transfer',
-      inputs: [
+      "type": "function",
+      "name": "transfer",
+      "inputs": [
         {
-          name: 'recipient',
-          type: 'address',
-          internalType: 'address',
+          "name": "recipient",
+          "type": "address",
+          "internalType": "address"
         },
         {
-          name: 'amount',
-          type: 'uint256',
-          internalType: 'uint256',
-        },
+          "name": "amount",
+          "type": "uint256",
+          "internalType": "uint256"
+        }
       ],
-      outputs: [],
-      stateMutability: 'nonpayable',
+      "outputs": [],
+      "stateMutability": "nonpayable"
     },
     {
-      type: 'event',
-      name: 'Transfer',
-      inputs: [
+      "type": "event",
+      "name": "Transfer",
+      "inputs": [
         {
-          name: 'from',
-          type: 'address',
-          indexed: true,
-          internalType: 'address',
+          "name": "from",
+          "type": "address",
+          "indexed": true,
+          "internalType": "address"
         },
         {
-          name: 'to',
-          type: 'address',
-          indexed: false,
-          internalType: 'address',
+          "name": "to",
+          "type": "address",
+          "indexed": false,
+          "internalType": "address"
         },
         {
-          name: 'value',
-          type: 'uint256',
-          indexed: false,
-          internalType: 'uint256',
-        },
+          "name": "value",
+          "type": "uint256",
+          "indexed": false,
+          "internalType": "uint256"
+        }
       ],
-      anonymous: false,
-    },
+      "anonymous": false
+    }
   ],
-  bytecode: {
-    object:
-      '0x6080604052348015600f57600080fd5b5060f28061001e6000396000f3fe6080604052348015600f57600080fd5b506004361060285760003560e01c8063a9059cbb14602d575b600080fd5b603c60383660046086565b603e565b005b604080516001600160a01b03841681526020810183905233917fddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef910160405180910390a25050565b60008060408385031215609857600080fd5b82356001600160a01b038116811460ae57600080fd5b94602093909301359350505056fea2646970667358221220a6f3eb2149eddb26ce1ca38a1c737e00a416c0f3d64aba593bd108ca0ea0da4264736f6c634300081b0033',
-    sourceMap: '64:296:3:-:0;;;;;;;;;;;;;;;;;;;',
-    linkReferences: {},
-  },
-} as const
+  "bytecode": {
+    "object": "0x6080604052348015600f57600080fd5b5060f28061001e6000396000f3fe6080604052348015600f57600080fd5b506004361060285760003560e01c8063a9059cbb14602d575b600080fd5b603c60383660046086565b603e565b005b604080516001600160a01b03841681526020810183905233917fddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef910160405180910390a25050565b60008060408385031215609857600080fd5b82356001600160a01b038116811460ae57600080fd5b94602093909301359350505056fea2646970667358221220a6f3eb2149eddb26ce1ca38a1c737e00a416c0f3d64aba593bd108ca0ea0da4264736f6c634300081b0033",
+    "sourceMap": "64:296:3:-:0;;;;;;;;;;;;;;;;;;;",
+    "linkReferences": {}
+  }
+} as const;
 
 export const Events = {
-  abi: [
+  "abi": [
     {
-      type: 'function',
-      name: 'execute',
-      inputs: [],
-      outputs: [],
-      stateMutability: 'payable',
+      "type": "function",
+      "name": "execute",
+      "inputs": [],
+      "outputs": [],
+      "stateMutability": "payable"
     },
     {
-      type: 'event',
-      name: 'MessageEmitted',
-      inputs: [
+      "type": "event",
+      "name": "MessageEmitted",
+      "inputs": [
         {
-          name: 'to',
-          type: 'address',
-          indexed: true,
-          internalType: 'address',
+          "name": "to",
+          "type": "address",
+          "indexed": true,
+          "internalType": "address"
         },
         {
-          name: 'value',
-          type: 'uint256',
-          indexed: false,
-          internalType: 'uint256',
+          "name": "value",
+          "type": "uint256",
+          "indexed": false,
+          "internalType": "uint256"
         },
         {
-          name: 'data',
-          type: 'bytes',
-          indexed: false,
-          internalType: 'bytes',
-        },
+          "name": "data",
+          "type": "bytes",
+          "indexed": false,
+          "internalType": "bytes"
+        }
       ],
-      anonymous: false,
-    },
+      "anonymous": false
+    }
   ],
-  bytecode: {
-    object:
-      '0x6080604052348015600f57600080fd5b5060db80601d6000396000f3fe608060405260043610601c5760003560e01c806361461954146021575b600080fd5b60276029565b005b336001600160a01b03167f7b2cff6dbed2a9cdb935eb6c46afadba8c4436a7aef48222ff62fce1ece4fcf334600036604051606593929190606f565b60405180910390a2565b83815260406020820152816040820152818360608301376000818301606090810191909152601f909201601f191601019291505056fea26469706673582212206d1e85a38a5863988fefa43ec3ed9d36562fcfae226c9888152dd6b0cbd0bf0064736f6c634300081b0033',
-    sourceMap: '64:203:2:-:0;;;;;;;;;;;;;;;;;;;',
-    linkReferences: {},
-  },
-} as const
+  "bytecode": {
+    "object": "0x6080604052348015600f57600080fd5b5060db80601d6000396000f3fe608060405260043610601c5760003560e01c806361461954146021575b600080fd5b60276029565b005b336001600160a01b03167f7b2cff6dbed2a9cdb935eb6c46afadba8c4436a7aef48222ff62fce1ece4fcf334600036604051606593929190606f565b60405180910390a2565b83815260406020820152816040820152818360608301376000818301606090810191909152601f909201601f191601019291505056fea26469706673582212206d1e85a38a5863988fefa43ec3ed9d36562fcfae226c9888152dd6b0cbd0bf0064736f6c634300081b0033",
+    "sourceMap": "64:203:2:-:0;;;;;;;;;;;;;;;;;;;",
+    "linkReferences": {}
+  }
+} as const;
 
 export const Errors = {
-  abi: [
+  "abi": [
     {
-      type: 'function',
-      name: 'assertRead',
-      inputs: [],
-      outputs: [],
-      stateMutability: 'pure',
+      "type": "function",
+      "name": "assertRead",
+      "inputs": [],
+      "outputs": [],
+      "stateMutability": "pure"
     },
     {
-      type: 'function',
-      name: 'assertWrite',
-      inputs: [],
-      outputs: [],
-      stateMutability: 'nonpayable',
+      "type": "function",
+      "name": "assertWrite",
+      "inputs": [],
+      "outputs": [],
+      "stateMutability": "nonpayable"
     },
     {
-      type: 'function',
-      name: 'complexCustomRead',
-      inputs: [],
-      outputs: [],
-      stateMutability: 'pure',
+      "type": "function",
+      "name": "complexCustomRead",
+      "inputs": [],
+      "outputs": [],
+      "stateMutability": "pure"
     },
     {
-      type: 'function',
-      name: 'complexCustomWrite',
-      inputs: [],
-      outputs: [],
-      stateMutability: 'nonpayable',
+      "type": "function",
+      "name": "complexCustomWrite",
+      "inputs": [],
+      "outputs": [],
+      "stateMutability": "nonpayable"
     },
     {
-      type: 'function',
-      name: 'divideByZeroRead',
-      inputs: [],
-      outputs: [
+      "type": "function",
+      "name": "divideByZeroRead",
+      "inputs": [],
+      "outputs": [
         {
-          name: '',
-          type: 'uint256',
-          internalType: 'uint256',
-        },
+          "name": "",
+          "type": "uint256",
+          "internalType": "uint256"
+        }
       ],
-      stateMutability: 'pure',
+      "stateMutability": "pure"
     },
     {
-      type: 'function',
-      name: 'divideByZeroWrite',
-      inputs: [],
-      outputs: [
+      "type": "function",
+      "name": "divideByZeroWrite",
+      "inputs": [],
+      "outputs": [
         {
-          name: '',
-          type: 'uint256',
-          internalType: 'uint256',
-        },
+          "name": "",
+          "type": "uint256",
+          "internalType": "uint256"
+        }
       ],
-      stateMutability: 'nonpayable',
+      "stateMutability": "nonpayable"
     },
     {
-      type: 'function',
-      name: 'overflowRead',
-      inputs: [],
-      outputs: [
+      "type": "function",
+      "name": "overflowRead",
+      "inputs": [],
+      "outputs": [
         {
-          name: '',
-          type: 'uint256',
-          internalType: 'uint256',
-        },
+          "name": "",
+          "type": "uint256",
+          "internalType": "uint256"
+        }
       ],
-      stateMutability: 'pure',
+      "stateMutability": "pure"
     },
     {
-      type: 'function',
-      name: 'overflowWrite',
-      inputs: [],
-      outputs: [
+      "type": "function",
+      "name": "overflowWrite",
+      "inputs": [],
+      "outputs": [
         {
-          name: '',
-          type: 'uint256',
-          internalType: 'uint256',
-        },
+          "name": "",
+          "type": "uint256",
+          "internalType": "uint256"
+        }
       ],
-      stateMutability: 'nonpayable',
+      "stateMutability": "nonpayable"
     },
     {
-      type: 'function',
-      name: 'requireRead',
-      inputs: [],
-      outputs: [],
-      stateMutability: 'pure',
+      "type": "function",
+      "name": "requireRead",
+      "inputs": [],
+      "outputs": [],
+      "stateMutability": "pure"
     },
     {
-      type: 'function',
-      name: 'requireWrite',
-      inputs: [],
-      outputs: [],
-      stateMutability: 'nonpayable',
+      "type": "function",
+      "name": "requireWrite",
+      "inputs": [],
+      "outputs": [],
+      "stateMutability": "nonpayable"
     },
     {
-      type: 'function',
-      name: 'revertRead',
-      inputs: [],
-      outputs: [],
-      stateMutability: 'pure',
+      "type": "function",
+      "name": "revertRead",
+      "inputs": [],
+      "outputs": [],
+      "stateMutability": "pure"
     },
     {
-      type: 'function',
-      name: 'revertWrite',
-      inputs: [],
-      outputs: [],
-      stateMutability: 'nonpayable',
+      "type": "function",
+      "name": "revertWrite",
+      "inputs": [],
+      "outputs": [],
+      "stateMutability": "nonpayable"
     },
     {
-      type: 'function',
-      name: 'simpleCustomRead',
-      inputs: [],
-      outputs: [],
-      stateMutability: 'pure',
+      "type": "function",
+      "name": "simpleCustomRead",
+      "inputs": [],
+      "outputs": [],
+      "stateMutability": "pure"
     },
     {
-      type: 'function',
-      name: 'simpleCustomReadNoArgs',
-      inputs: [],
-      outputs: [],
-      stateMutability: 'pure',
+      "type": "function",
+      "name": "simpleCustomReadNoArgs",
+      "inputs": [],
+      "outputs": [],
+      "stateMutability": "pure"
     },
     {
-      type: 'function',
-      name: 'simpleCustomWrite',
-      inputs: [],
-      outputs: [],
-      stateMutability: 'nonpayable',
+      "type": "function",
+      "name": "simpleCustomWrite",
+      "inputs": [],
+      "outputs": [],
+      "stateMutability": "nonpayable"
     },
     {
-      type: 'error',
-      name: 'ComplexError',
-      inputs: [
+      "type": "error",
+      "name": "ComplexError",
+      "inputs": [
         {
-          name: 'foo',
-          type: 'tuple',
-          internalType: 'struct Errors.Foo',
-          components: [
+          "name": "foo",
+          "type": "tuple",
+          "internalType": "struct Errors.Foo",
+          "components": [
             {
-              name: 'sender',
-              type: 'address',
-              internalType: 'address',
+              "name": "sender",
+              "type": "address",
+              "internalType": "address"
             },
             {
-              name: 'bar',
-              type: 'uint256',
-              internalType: 'uint256',
-            },
-          ],
+              "name": "bar",
+              "type": "uint256",
+              "internalType": "uint256"
+            }
+          ]
         },
         {
-          name: 'message',
-          type: 'string',
-          internalType: 'string',
+          "name": "message",
+          "type": "string",
+          "internalType": "string"
         },
         {
-          name: 'number',
-          type: 'uint256',
-          internalType: 'uint256',
-        },
-      ],
+          "name": "number",
+          "type": "uint256",
+          "internalType": "uint256"
+        }
+      ]
     },
     {
-      type: 'error',
-      name: 'SimpleError',
-      inputs: [
+      "type": "error",
+      "name": "SimpleError",
+      "inputs": [
         {
-          name: 'message',
-          type: 'string',
-          internalType: 'string',
-        },
-      ],
+          "name": "message",
+          "type": "string",
+          "internalType": "string"
+        }
+      ]
     },
     {
-      type: 'error',
-      name: 'SimpleErrorNoArgs',
-      inputs: [],
-    },
+      "type": "error",
+      "name": "SimpleErrorNoArgs",
+      "inputs": []
+    }
   ],
-  bytecode: {
-    object:
-      '0x6080604052348015600f57600080fd5b506103228061001f6000396000f3fe608060405234801561001057600080fd5b50600436106100f55760003560e01c80638de18b9111610097578063c66cf13311610066578063c66cf1331461010c578063d44de8661461012e578063eb1aba20146100fa578063efbbf9951461014657600080fd5b80638de18b9114610104578063940b88021461013e5780639f5587091461013e578063a997732e1461013657600080fd5b80634a9bc278116100d35780634a9bc278146101265780634adac6eb1461012e578063699389ca1461012657806388452b851461013657600080fd5b806304696152146100fa5780631515d7681461010457806324db9ba01461010c575b600080fd5b61010261014e565b005b610102610158565b6101146101c3565b60405190815260200160405180910390f35b610102600080fd5b6101146101db565b6101026101ed565b610102610206565b61010261024e565b610156610267565b565b6040805180820182526000815260456020820181815292516336dcc73d60e21b815291516001600160a01b0316600483015291516024820152608060448201526006608482015265313ab3b3b2b960d11b60a4820152606481019190915260c4015b60405180910390fd5b6000604581806101d3818461027d565b949350505050565b60006000196001826101d3828461029f565b604051631f200c7360e31b81526004016101ba906102c6565b60405162461bcd60e51b815260206004820152601860248201527f54686973206973206120726576657274206d657373616765000000000000000060448201526064016101ba565b6040516333a3b5cd60e11b815260040160405180910390fd5b634e487b7160e01b600052600160045260246000fd5b60008261029a57634e487b7160e01b600052601260045260246000fd5b500490565b808201808211156102c057634e487b7160e01b600052601160045260246000fd5b92915050565b6020815260006102c0602083016006815265313ab3b3b2b960d11b60208201526040019056fea2646970667358221220715dbc4289554348f035ca76d06003c1d3ffa481d5cb3d413f03aa0f011cf4e664736f6c634300081b0033',
-    sourceMap: '64:2024:1:-:0;;;;;;;;;;;;;;;;;;;',
-    linkReferences: {},
-  },
-} as const
+  "bytecode": {
+    "object": "0x6080604052348015600f57600080fd5b506103228061001f6000396000f3fe608060405234801561001057600080fd5b50600436106100f55760003560e01c80638de18b9111610097578063c66cf13311610066578063c66cf1331461010c578063d44de8661461012e578063eb1aba20146100fa578063efbbf9951461014657600080fd5b80638de18b9114610104578063940b88021461013e5780639f5587091461013e578063a997732e1461013657600080fd5b80634a9bc278116100d35780634a9bc278146101265780634adac6eb1461012e578063699389ca1461012657806388452b851461013657600080fd5b806304696152146100fa5780631515d7681461010457806324db9ba01461010c575b600080fd5b61010261014e565b005b610102610158565b6101146101c3565b60405190815260200160405180910390f35b610102600080fd5b6101146101db565b6101026101ed565b610102610206565b61010261024e565b610156610267565b565b6040805180820182526000815260456020820181815292516336dcc73d60e21b815291516001600160a01b0316600483015291516024820152608060448201526006608482015265313ab3b3b2b960d11b60a4820152606481019190915260c4015b60405180910390fd5b6000604581806101d3818461027d565b949350505050565b60006000196001826101d3828461029f565b604051631f200c7360e31b81526004016101ba906102c6565b60405162461bcd60e51b815260206004820152601860248201527f54686973206973206120726576657274206d657373616765000000000000000060448201526064016101ba565b6040516333a3b5cd60e11b815260040160405180910390fd5b634e487b7160e01b600052600160045260246000fd5b60008261029a57634e487b7160e01b600052601260045260246000fd5b500490565b808201808211156102c057634e487b7160e01b600052601160045260246000fd5b92915050565b6020815260006102c0602083016006815265313ab3b3b2b960d11b60208201526040019056fea2646970667358221220715dbc4289554348f035ca76d06003c1d3ffa481d5cb3d413f03aa0f011cf4e664736f6c634300081b0033",
+    "sourceMap": "64:2024:1:-:0;;;;;;;;;;;;;;;;;;;",
+    "linkReferences": {}
+  }
+} as const;
 
 export const Constructor = {
-  abi: [
+  "abi": [
     {
-      type: 'constructor',
-      inputs: [
+      "type": "constructor",
+      "inputs": [
         {
-          name: '_address',
-          type: 'address',
-          internalType: 'address',
+          "name": "_address",
+          "type": "address",
+          "internalType": "address"
         },
         {
-          name: '_number',
-          type: 'uint256',
-          internalType: 'uint256',
-        },
+          "name": "_number",
+          "type": "uint256",
+          "internalType": "uint256"
+        }
       ],
-      stateMutability: 'nonpayable',
-    },
+      "stateMutability": "nonpayable"
+    }
   ],
-  bytecode: {
-    object:
-      '0x6080604052348015600f57600080fd5b5060405160b438038060b4833981016040819052602a916030565b50506068565b60008060408385031215604257600080fd5b82516001600160a01b0381168114605857600080fd5b6020939093015192949293505050565b603f8060756000396000f3fe6080604052600080fdfea2646970667358221220e362025fc510dd0907d5ac6edd999dca454327eab3287e8c4a74e9881904a77d64736f6c634300081b0033',
-    sourceMap:
-      '64:78:0:-:0;;;91:49;;;;;;;;;;;;;;;;;;;;;;;;;;;;:::i;:::-;;;64:78;;14:351:4;93:6;101;154:2;142:9;133:7;129:23;125:32;122:52;;;170:1;167;160:12;122:52;196:16;;-1:-1:-1;;;;;241:31:4;;231:42;;221:70;;287:1;284;277:12;221:70;355:2;340:18;;;;334:25;310:5;;334:25;;-1:-1:-1;;;14:351:4:o;:::-;64:78:0;;;;;;',
-    linkReferences: {},
-  },
-} as const
+  "bytecode": {
+    "object": "0x6080604052348015600f57600080fd5b5060405160b438038060b4833981016040819052602a916030565b50506068565b60008060408385031215604257600080fd5b82516001600160a01b0381168114605857600080fd5b6020939093015192949293505050565b603f8060756000396000f3fe6080604052600080fdfea2646970667358221220e362025fc510dd0907d5ac6edd999dca454327eab3287e8c4a74e9881904a77d64736f6c634300081b0033",
+    "sourceMap": "64:78:0:-:0;;;91:49;;;;;;;;;;;;;;;;;;;;;;;;;;;;:::i;:::-;;;64:78;;14:351:4;93:6;101;154:2;142:9;133:7;129:23;125:32;122:52;;;170:1;167;160:12;122:52;196:16;;-1:-1:-1;;;;;241:31:4;;231:42;;221:70;;287:1;284;277:12;221:70;355:2;340:18;;;;334:25;310:5;;334:25;;-1:-1:-1;;;14:351:4:o;:::-;64:78:0;;;;;;",
+    "linkReferences": {}
+  }
+} as const;
+
