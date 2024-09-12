9d75378f9f629d6c6ec6a30dd1a77b83228e50e0
Will Cory <willcory10@gmail.com>
2024-09-12 00:21:33 -0700

    add announce provider types

--- src/window/index.ts
+++ src/window/index.ts
@@ -1,7 +1,22 @@
 import type { Provider } from '../internal/Provider/types.js'
 
+// TODO add this
+type EIP1193Provider = any
+
 declare global {
   interface Window {
     ethereum?: Provider | undefined
+  }
+  interface WindowEventMap {
+    'eip6963:requestProvider': Event & {type: 'eip6963:requestProvider'}
+    'eip6963:announceProvider': CustomEvent<{
+      info:   {
+        icon: `data:image/${string}`
+        name: string
+        rdns: string
+        uuid: string
+      }
+      provider: EIP1193Provider
+    }> & {type: 'eip6963:announceProvider'}
   }
 }
