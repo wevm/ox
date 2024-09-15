e78a098d6cd2a72ac36fcd870107c3f42a623da8
Kyle Scott <kyscott@umich.edu>
2024-09-14 21:35:09 -0400

    bloom

---
+++ src/internal/Bloom/contains.ts
@@ -0,0 +1,47 @@
+import type { GlobalErrorType } from '../Errors/error.js'
+import { Bytes_from } from '../Bytes/from.js'
+import type { Bytes } from '../Bytes/types.js'
+import { Hash_keccak256 } from '../Hash/keccak256.js'
+import type { Hex } from '../Hex/types.js'
+
+/**
+ * Checks if an input is matched in the bloom filter.
+ *
+ *  * @example
+ * ```ts twoslash
+ * import { Bloom } from 'ox'
+ *
+ * Bloom.contains(
+ *   '0x00000000000000000000000000000000000000001000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000002020000000000000000000000000000000000000000000008000000001000000000000000000000000000000000000000000000000000001000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000',
+ *   '0xef2d6d194084c2de36e0dabfce45d046b37d1106',
+ * )
+ * // @log: true
+ * ```
+ *
+ * @param bloom - Bloom filter value.
+ * @param input - Input to check.
+ * @returns Whether the input is matched in the bloom filter.
+ */
+export function Bloom_contains(bloom: Hex, input: Hex | Bytes): boolean {
+  const filter = Bytes_from(bloom)
+  const hash = Hash_keccak256(input, 'Bytes')
+
+  for (const i of [0, 2, 4]) {
+    const bit = (hash[i + 1]! + (hash[i]! << 8)) & 0x7ff
+    if ((filter[256 - 1 - Math.floor(bit / 8)]! & (1 << (bit % 8))) === 0)
+      return false
+  }
+
+  return true
+}
+
+export declare namespace Bloom_contains {
+  type ErrorType =
+    | Bytes_from.ErrorType
+    | Hash_keccak256.ErrorType
+    | GlobalErrorType
+}
+
+/* v8 ignore next */
+Bloom_contains.parseError = (error: unknown) =>
+  error as Bloom_contains.ErrorType
--- src/internal/Hash/isHash.test.ts
+++ src/internal/Hash/isHash.test.ts
@@ -1,7 +1,7 @@
 import { Hash } from 'ox'
 import { expect, test } from 'vitest'
 
-test('checks if address is valid', () => {
+test('checks if hash is valid', () => {
   expect(Hash.isHash('0xa5cc3c03994db5b0d9a5eEdD10Cabab0813678ac')).toBeFalsy()
   expect(Hash.isHash('0xa0cf798816d4b9b9866b5330eea46a18382f251e')).toBeFalsy()
   expect(Hash.isHash('0xa5cc3c03994db5b0d9a5eEdD10Cabab0813678az')).toBeFalsy()
