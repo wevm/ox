3de2b1cc58aa47c6f9e8663ca8581dabc0ce1579
jxom <jakemoxey@gmail.com>
2024-09-17 08:41:53 +1000

    fix: preconstruct script

--- scripts/prepublish.ts
+++ scripts/prepublish.ts
@@ -1,7 +1,6 @@
 import { join, relative, resolve } from 'node:path'
 import fs from 'fs-extra'
-
-const ignore = ['node_modules', '_dist']
+import { getExports } from './utils/exports.js'
 
 const packageJsonPath = join(import.meta.dirname, '../src/package.json')
 const packageJson = fs.readJsonSync(packageJsonPath)
@@ -12,109 +11,35 @@ const jsrJson = fs.readJsonSync(jsrJsonPath)
 // We don't want to publish the type field.
 delete packageJson.type
 
-const entries = fs.readdirSync(resolve(import.meta.dirname, '../src'), {
-  withFileTypes: true,
+const exports = getExports({
+  onEntry: ({ entryName, name, parentEntryName }) => {
+    const distBasePath = parentEntryName
+      ? `./_dist/${parentEntryName}/${name}`
+      : `./_dist/${name}`
+
+    try {
+      fs.mkdirSync(resolve(import.meta.dirname, '../src', entryName))
+    } catch {}
+    fs.writeJsonSync(
+      resolve(import.meta.dirname, '../src', entryName, 'package.json'),
+      {
+        type: 'module',
+        types: relative(
+          resolve(import.meta.dirname, '../src'),
+          `${distBasePath}.d.ts`,
+        ),
+        main: relative(
+          resolve(import.meta.dirname, '../src'),
+          `${distBasePath}.js`,
+        ),
+      },
+      { spaces: 2 },
+    )
+  },
 })
 
-// Set up exports.
-packageJson.exports = {}
-jsrJson.exports = {}
-for (const parentEntry of entries) {
-  if (ignore.includes(parentEntry.name)) continue
-
-  if (!parentEntry.isDirectory()) {
-    if (parentEntry.name.endsWith('test.ts')) continue
-    if (parentEntry.name === 'jsr.json') continue
-    if (
-      !parentEntry.name.endsWith('.ts') &&
-      !parentEntry.name.endsWith('.json')
-    )
-      continue
-
-    const name = parentEntry.name.replace(/\.ts$/, '')
-    const entryName = `.${name === 'index' ? '' : `/${name}`}`
-
-    if (!parentEntry.name.endsWith('.ts')) {
-      packageJson.exports[entryName] = entryName
-      jsrJson.exports[entryName] = entryName
-    } else {
-      packageJson.exports[entryName] = {
-        types: `./_dist/${name}.d.ts`,
-        default: `./_dist/${name}.js`,
-      }
-      jsrJson.exports[entryName] = `./${name}.ts`
-
-      if (name === 'index') continue
-
-      try {
-        fs.mkdirSync(resolve(import.meta.dirname, '../src', entryName))
-      } catch {}
-      fs.writeJsonSync(
-        resolve(import.meta.dirname, '../src', entryName, 'package.json'),
-        {
-          type: 'module',
-          types: relative(
-            resolve(import.meta.dirname, '../src'),
-            `./_dist/${name}.d.ts`,
-          ),
-          main: relative(
-            resolve(import.meta.dirname, '../src'),
-            `./_dist/${name}.js`,
-          ),
-        },
-        { spaces: 2 },
-      )
-    }
-
-    continue
-  }
-
-  const entries = fs.readdirSync(
-    resolve(parentEntry.parentPath, parentEntry.name),
-    {
-      withFileTypes: true,
-    },
-  )
-  for (const entry of entries) {
-    if (ignore.includes(entry.name)) continue
-    if (entry.name.endsWith('test.ts')) continue
-    if (entry.isDirectory()) continue
-
-    const isIndex = entry.name === 'index.ts'
-    const name = entry.name.replace(/\.ts$/, '')
-
-    if (!/^[A-Z]/.test(name) && name !== 'index') continue
-
-    const entryName = `.${parentEntry.name === 'core' ? '' : `/${parentEntry.name}`}${isIndex ? '' : `/${name}`}`
-
-    packageJson.exports[entryName] = {
-      types: `./_dist/${parentEntry.name}/${name}.d.ts`,
-      default: `./_dist/${parentEntry.name}/${name}.js`,
-    }
-    jsrJson.exports[entryName] = `./${parentEntry.name}/${name}.ts`
-
-    if (entryName !== '.') {
-      try {
-        fs.mkdirSync(resolve(import.meta.dirname, '../src', entryName))
-      } catch {}
-      fs.writeJsonSync(
-        resolve(import.meta.dirname, '../src', entryName, 'package.json'),
-        {
-          type: 'module',
-          types: relative(
-            resolve(import.meta.dirname, '../src'),
-            `./_dist/${parentEntry.name}/${name}.d.ts`,
-          ),
-          main: relative(
-            resolve(import.meta.dirname, '../src'),
-            `./_dist/${parentEntry.name}/${name}.js`,
-          ),
-        },
-        { spaces: 2 },
-      )
-    }
-  }
-}
+packageJson.exports = exports.dist
+jsrJson.exports = exports.src
 
 fs.writeJsonSync(packageJsonPath, packageJson, { spaces: 2 })
 fs.writeJsonSync(jsrJsonPath, jsrJson, { spaces: 2 })
