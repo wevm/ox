d9f26c9f98a1301c06e736e339ec81cf3eef8f39
jxom <jakemoxey@gmail.com>
2024-10-20 13:22:43 +1100

    chore: up

--- src/TypedData.test.ts
+++ src/TypedData.test.ts
@@ -7,6 +7,7 @@ test('exports', () => {
     [
       "BytesSizeMismatchError",
       "InvalidPrimaryTypeError",
+      "InvalidStructTypeError",
       "assert",
       "domainSeparator",
       "encodeType",
