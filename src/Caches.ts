f501e33cf72a34acb014a397108a5728ea17de7a
Tom Meagher <tom@meagher.co>
2024-10-29 17:26:44 -0400

    chore: convert

--- src/Caches.ts
+++ src/Caches.ts
@@ -1,4 +1,21 @@
-export {
-  Caches_checksum as checksum,
-  Caches_clear as clear,
-} from './internal/caches.js'
+import type * as Address from './Address.js'
+import { LruMap } from './internal/lru.js'
+
+const caches = {
+  checksum: /*#__PURE__*/ new LruMap<Address.Address>(8192),
+}
+
+export const checksum = caches.checksum
+
+/**
+ * Clears all global caches.
+ *
+ * @example
+ * ```ts
+ * import { Caches } from 'ox'
+ * Caches.clear()
+ * ```
+ */
+export function clear() {
+  for (const cache of Object.values(caches)) cache.clear()
+}
