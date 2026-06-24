5fa3b50727f77c871aabd32bbfc340aa3c4251fb
Derek <256792747+decofe@users.noreply.github.com>
2026-06-24 20:02:20 +0000

    Document TokenAddress helper

--- src/tempo/TokenAddress.ts
+++ src/tempo/TokenAddress.ts
@@ -8,6 +8,14 @@ export const prefix = '0x20c000000000000000000000'
  *
  * [TIP-20 Token Standard](https://docs.tempo.xyz/protocol/tip20/overview)
  *
+ * @example
+ * ```ts twoslash
+ * import { TokenAddress } from 'ox/tempo'
+ *
+ * const isTip20 = TokenAddress.isTip20('0x20c0000000000000000000000000000000000001')
+ * // @log: true
+ * ```
+ *
  * @param address - Address to check.
  * @returns Whether the address is a TIP-20 token address.
  */
