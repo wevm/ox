5def70f583eed3bbe44ea6bbebff45e82f2eb07f
jxom <7336481+jxom@users.noreply.github.com>
2026-09-26 21:47:11 +1000

    fix(siwe): parse resources from the resources section

--- src/core/Siwe.ts
+++ src/core/Siwe.ts
@@ -21,7 +21,7 @@ export const prefixRegex =
 
 // https://regexr.com/80gf9
 export const suffixRegex =
-  /(?:URI: (?<uri>.+))\n(?:Version: (?<version>.+))\n(?:Chain ID: (?<chainId>\d+))\n(?:Nonce: (?<nonce>[a-zA-Z0-9]+))\n(?:Issued At: (?<issuedAt>.+))(?:\nExpiration Time: (?<expirationTime>.+))?(?:\nNot Before: (?<notBefore>.+))?(?:\nRequest ID: (?<requestId>.+))?/
+  /(?:URI: (?<uri>.+))\n(?:Version: (?<version>.+))\n(?:Chain ID: (?<chainId>\d+))\n(?:Nonce: (?<nonce>[a-zA-Z0-9]+))\n(?:Issued At: (?<issuedAt>.+))(?:\nExpiration Time: (?<expirationTime>.+))?(?:\nNot Before: (?<notBefore>.+))?(?:\nRequest ID: (?<requestId>.*))?(?:\nResources:(?<resources>(?:\n- .+)*))?/
 
 /** [EIP-4361](https://eips.ethereum.org/EIPS/eip-4361) message fields. */
 export type Message = {
@@ -372,18 +372,26 @@ export function parseMessage(message: string): ExactPartial<Message> {
     scheme?: string
     statement?: string
   }
-  const { chainId, expirationTime, issuedAt, notBefore, requestId, ...suffix } =
-    (message.match(suffixRegex)?.groups ?? {}) as {
-      chainId: string
-      expirationTime?: string
-      issuedAt?: string
-      nonce: string
-      notBefore?: string
-      requestId?: string
-      uri: string
-      version: '1'
-    }
-  const resources = message.split('Resources:')[1]?.split('\n- ').slice(1)
+  const {
+    chainId,
+    expirationTime,
+    issuedAt,
+    notBefore,
+    requestId,
+    resources: resources_,
+    ...suffix
+  } = (message.match(suffixRegex)?.groups ?? {}) as {
+    chainId: string
+    expirationTime?: string
+    issuedAt?: string
+    nonce: string
+    notBefore?: string
+    requestId?: string
+    resources?: string
+    uri: string
+    version: '1'
+  }
+  const resources = resources_?.split('\n- ').slice(1)
   return {
     ...prefix,
     ...suffix,
